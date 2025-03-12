ff360e83e7d12e4b9752a2d190ee2654fcf517b8
Huawei <iralanda@mail.ru>
2025-03-12 20:02:35 +0300

    My homework is done

--- src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/kotlin/homework/network/NetworkLogger.kt
@@ -30,6 +30,10 @@ class ErrorLogger<E : Throwable> {
             println("Error at $date: ${error.message}")
         }
     }
+
+    fun dump(): List<Pair<LocalDateTime, E>> {
+        return errors
+    }
 }
 
 fun processThrowables(logger: ErrorLogger<Throwable>) {
@@ -42,7 +46,7 @@ fun processThrowables(logger: ErrorLogger<Throwable>) {
     logger.dumpLog()
 }
 
-fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
+fun processApiErrors(apiExceptionLogger: ErrorLogger<in ApiException>) {
     apiExceptionLogger.log(Success("Success"))
     Thread.sleep(100)
     apiExceptionLogger.log(Success(Circle))
@@ -52,6 +56,7 @@ fun processApiErrors(apiExceptionLogger: ErrorLogger<ApiException>) {
     apiExceptionLogger.dumpLog()
 }
 
+
 fun main() {
     val logger = ErrorLogger<Throwable>()
 
@@ -60,5 +65,11 @@ fun main() {
 
     println("Processing Api:")
     processApiErrors(logger)
+
+    val errorList = logger.dump()
+    println("List of errors:")
+    errorList.forEach { (date, error) ->
+        println("Error at $date: ${error.message}")
+    }
 }
 
